78b8ff4c880afcf3bf20d6e301df55d549ffdd63
Gaurav28102 <143395580+Gaurav28102@users.noreply.github.com>
2023-09-20 10:17:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express=require("express");
+const app=express()
+
+const PORT=3000;
+app.get("/",(req,res)=>{
+    console.log("Hello This is welcome page");
+    res.send("Welcome");
+})
+app.listen(PORT,(req,res)=>{
+    console.log(`Server running at port ${PORT}`);
+})
