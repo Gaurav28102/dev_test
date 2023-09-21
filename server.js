c7eeee7705e5b94d27b36c93dc283ae1bcbfd207
Gaurav28102 <143395580+Gaurav28102@users.noreply.github.com>
2023-09-21 15:17:53 +0530

    Update server.js 1

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app=express()
 const PORT=3000;
 app.get("/",(req,res)=>{
     console.log("Hello This is welcome page");
-    res.send("Welcome to Dashboard");
+    res.send("Welcome to Dashboard Part 1");
 })
 app.listen(PORT,(req,res)=>{
     console.log(`Server running at port ${PORT}`);
